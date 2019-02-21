c55cb78eb7f398e00d8f40fa95df2fb8f59db445
Zlatko Bratkovic <zbratkovic@haproxy.com>
2019-02-21 09:46:16 +0100

    TESTS: balance uri tests added

--- tests/balance_autogenerated_test.go
+++ tests/balance_autogenerated_test.go
@@ -31,6 +31,48 @@ func TestBalanceNormal0(t *testing.T) {
 		t.Errorf(fmt.Sprintf("error: has [%s] expects [%s]", returnLine, line))
 	}
 }
+func TestBalanceNormal1(t *testing.T) {
+	parser := &parsers.Balance{}
+	line := strings.TrimSpace("balance uri depth 8")
+	err := ProcessLine(line, parser)
+	if err != nil {
+		t.Errorf(err.Error())
+	}
+	result, err := parser.Result(true)
+	if err != nil {
+		t.Errorf(err.Error())
+	}
+	var returnLine string
+	if result[0].Comment == "" {
+		returnLine = fmt.Sprintf("%s", result[0].Data)
+	} else {
+		returnLine = fmt.Sprintf("%s # %s", result[0].Data, result[0].Comment)
+	}
+	if line != returnLine {
+		t.Errorf(fmt.Sprintf("error: has [%s] expects [%s]", returnLine, line))
+	}
+}
+func TestBalanceNormal2(t *testing.T) {
+	parser := &parsers.Balance{}
+	line := strings.TrimSpace("balance uri")
+	err := ProcessLine(line, parser)
+	if err != nil {
+		t.Errorf(err.Error())
+	}
+	result, err := parser.Result(true)
+	if err != nil {
+		t.Errorf(err.Error())
+	}
+	var returnLine string
+	if result[0].Comment == "" {
+		returnLine = fmt.Sprintf("%s", result[0].Data)
+	} else {
+		returnLine = fmt.Sprintf("%s # %s", result[0].Data, result[0].Comment)
+	}
+	if line != returnLine {
+		t.Errorf(fmt.Sprintf("error: has [%s] expects [%s]", returnLine, line))
+	}
+}
 func TestBalanceFail0(t *testing.T) {
 	parser := &parsers.Balance{}
 	line := strings.TrimSpace("balance something")
