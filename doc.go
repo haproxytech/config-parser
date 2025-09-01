985aae77ad220866d878e96c83dbb8677b6257f2
Zlatko Bratkovic <zbratkovic@haproxy.com>
2025-09-01 09:45:11 +0200

    MAJOR: archive the project

---
+++ doc.go
@@ -0,0 +1,30 @@
+/*
+Copyright 2019 HAProxy Technologies
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+// Package parser is a configuration parser for HAProxy configuration file.
+//
+// Deprecated: This package is no longer maintained. Please migrate to
+// the config-parser package located at github.com/haproxytech/client-native/config-parser.
+//
+// This project has been merged with [client-native](https://github.com/haproxytech/client-native) project.
+// Example of usage change:
+//
+//	import "github.com/haproxytech/client-native/v6/config-parser"
+//
+// instead of
+//
+//	import "github.com/haproxytech/config-parser/v5"
+package parser
